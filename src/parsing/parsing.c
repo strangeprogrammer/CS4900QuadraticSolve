99417f082a2bfa6c66258ad283978315604e3d01
Daniel Darcy” <daniel.m.darcy@wmich.edu>
2018-11-10 14:47:26 -0500

    Fixed warning message generated fromm parsing.c. I think some lines got copied or overwritten somehow during merge/commit.

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -2,43 +2,36 @@
 #include "../misc.h"
 #include "parsing.h"
 
-#include <unistd.h>
-
 bool checkargs(int argc){
-	if(argc!=4){
-		fprintf(stderr,"Error: this program needs 3 arguments exactly.\n");
-		return false;
-	}
-	return true;
+    if(argc!=4){
+        fprintf(stderr,"Error: this program needs 3 arguments exactly.\n");
+        return false;
+    }
+    return true;
 }
 
-//Parse a float if possible, else return an error
 fpstatus getarg(char arg[]){
-	char* end;
-	errno=0;
-	float parsed=strtof(arg,&end);
-	fpstatus retval={.f=0, .e=SUCCESS};
-	if(parsed==0||fpclassify(parsed)==FP_ZERO){//If we got a 0 back
-		if(end==arg){//If strtof didn't convert anything
-			fprintf(stderr,"*Error: could not convert argument '%s' to a float.\n",arg);
-            fpstatus retval={.f=0,.e=SUCCESS};
+    char* end;
+    errno=0;
+    float parsed=strtof(arg,&end);
+    fpstatus retval={.f=0,.e=SUCCESS};
+    if(parsed==0||fpclassify(parsed)==FP_ZERO){
+        if(end==arg){
+            fprintf(stderr,"Error: could not convert argument '%s' to a float.\n",arg);
+            retval.e=BADARG_ERR;
+        }else if(errno==ERANGE){
+            fprintf(stderr,"Error: argument '%s' causes underflow.\n",arg);
+            retval.e=UNDERFLOW_ERR;
         }
+    }else if(isinf(parsed)){
+        fprintf(stderr,"Error: argument '%s' causes overflow.\n",arg);
+        retval.e=OVERFLOW_ERR;
+    }else if(isnan(parsed)){
+        fprintf(stderr,"Error: argument '%s' is not a number.\n",arg);
+        retval.e=NANARG_ERR;
     }
-	if(parsed==0||fpclassify(parsed)==FP_ZERO){
-		if(end==arg){
-			fprintf(stderr, "Error: could not convert argument '%s' to a float.\n",arg);
-			retval.e=BADARG_ERR;
-		}else if(errno==ERANGE){
-			fprintf(stderr, "Error: argument '%s' causes underflow.\n",arg);
-			retval.e=UNDERFLOW_ERR;
-		}
-	}else if(isinf(parsed)){
-		fprintf(stderr, "Error: argument '%s' causes overflow.\n",arg);
-		retval.e=OVERFLOW_ERR;
-	}else if(isnan(parsed)){
-		fprintf(stderr, "Error: argument '%s' is not a number.\n",arg);
-		retval.e=NANARG_ERR;
-	}
-	retval.f=parsed;//Huzzah, it worked!
-	return retval;
+    retval.f=parsed;
+    return retval;
 }
+
+//fpstatus.f is individual arguments
