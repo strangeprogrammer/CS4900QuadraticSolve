60860a1a60e49b20a6084e2fee027b7ff129d82a
Daniel Darcy” <daniel.m.darcy@wmich.edu>
2018-11-26 21:49:02 -0500

    Added system testing with script. Updated Tps_reports. Added system_testing to makefile. Finished documentation. Only missing Gantt chart.

---
+++ src/system_test_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+make
+
+#echo "System Test: "
+{
+./quadsolve "1" "2" "3"
+./quadsolve "1" "-6.4" "10.23"
+./quadsolve "1" "10" "25"
+./quadsolve "1" "-10" "30"
+./quadsolve "-.5" "2" "1"
+} > st_actual_output.txt
+diff st_actual_output.txt st_expected_output.txt &> system_test_log.txt
+if [ $? != 0 ]; then
+echo "SYSTEM TEST FAILED: Check sytem_test_log for more info."
+fi
+rm st_actual_output.txt
